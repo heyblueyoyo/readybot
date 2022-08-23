2f4ede738c5a6261c1040d3b824010eea91acb01
zijianguan0204 <31665442+zijianguan0204@users.noreply.github.com>
2022-08-23 01:41:14 -0700

    add score function for sending message with user profile

--- readybot.py
+++ readybot.py
@@ -1,16 +1,19 @@
+from asyncio.windows_events import NULL
+from contextlib import nullcontext
 import discord
 from discord.ext import commands
 import json
 import os
 import random
 
-#TOKEN='ODQ2ODYwMDM1NzgzMTMxMjE2.GJtpdX.7Ug3O1HB8-l0yPIJN-IQSpf6RPdVaDE6TW9cQQ'
-bot=commands.Bot(command_prefex='ready')
 
+intents = discord.Intents.all()
+intents.members = True
+bot = commands.Bot(command_prefix='ready', intents = intents)
 
 
 #bot commnads decorator can be used to register any bot.commands
-@bot.commands()
+@bot.command
 async def on_ready():
 	print('Ready Player积分商人在线营业')
 
@@ -24,37 +27,116 @@ async def on_member_join(member):
 	with open('users.json','w') as f:
 		json.dump(users,f)
 
-@bot.event
-async def on_message(message):
+# @bot.event
+# async def on_message(message):
+# 	with open('users.json','r') as f:
+# 		users=json.load(f)
+
+
+# 	await add_exp(users,message.author,1)
+# 	await update_data(users,message.author)
+# 	#send a message to server for level up
+# 	await check_level_up(users,message.author,message.channel)
+
+# 	with open('users.json','w') as f:
+# 		json.dump(users,f)
+
+
+def checkUser(author):
 	with open('users.json','r') as f:
 		users=json.load(f)
-
-
-	await add_exp(users,message.author,1)
-	await update_data(users,message.author)
-	#send a message to server for level up
-	await check_level_up(users,message.author,message.channel)
-
+	hasUser = False
+	if len(users) > 0:
+		for user in users:
+			if author.id == user["userId"]:
+				hasUser = True
+				continue
+	if hasUser == False :
+		username = author.display_name
+		userId = author.id
+		newUser = {
+			"username": username,
+			"userId" : userId,
+			"score": 0,
+			"level": "silver",
+			"time": NULL
+		}
+		users.append(newUser)
 	with open('users.json','w') as f:
 		json.dump(users,f)
 
+#TODO: return the score that the user can get from sending message
+# image : +10
+# text : +1
+def updateScore(message):
+	#check if the message is a pic
+	earnedPoints = 1
+	embeds = message.embeds
+	print(len(embeds))
+	if len(embeds) > 0:
+		for embed in embeds:
+			if embed.type == "image":
+				earnedPoints = 10
+				print("The embed type is image")
+	with open('users.json','r') as f:
+		users=json.load(f)
+	author = message.author
+	for user in users:
+		print("The original score is " + str(user["score"]))
+		score = user["score"] + earnedPoints
+		print("The updated score is " + str(score))
+		#TODO:opeartion regarding level up after adding the earned points
+		level = user["level"]
+		time = user["time"]
+		if author.id == user["userId"]:
+			user["score"] = score
+			user["level"] = level
+			user["time"] = time
+			print(user)
+		
+	with open('users.json','w') as f:
+		json.dump(users,f)
 
 @bot.event
 async def on_message(message):
-    if message.content == "使用":
-        await message.reply("Ready Playe 积分商人使用规则如下")
+	#ignore if the message is coming from the bot
+	if message.author.id == 846860035783131216:
+		return
+	#check if the user already exists in db/json, and the new user will be created if it does not exist
+	checkUser(message.author)
+	#check how many points can be added to the user regarding its sent message
+	updateScore(message)
+
+	channel = message.channel
+	if message.content == "使用":
+		await channel.send("Ready Player 积分商人使用规则如下")
+	if message.content == "info":
+		username = message.author.display_name
+		score = 0
+		level = "silver"
+		with open('users.json','r') as f:
+			users=json.load(f)
+		for user in users:
+			if message.author.id == user["userId"]:
+				score = user["score"]
+				level = user["level"]
+				continue
+		await channel.send("The current user is " + username)
+		await channel.send("The user score is " + str(score))
+		await channel.send("The user level is " + level)
+
 
 ##########3await voice channels
-# def on_voice_state_update(member,before,after):
-	#with open('users.json','r') as f:
-		#users=json.load(f)
-	#if not before.channel and after.channel:
-        #users[user.id] = time.time()
-    #elif before.channel and not after.channel and member.id in data:
-        #minutes = (time.time() - users[user.id]) // 60
-    #await add_exp(users,message.author,minutes)
-    #await update_data(users,message.author)
-   # await level_up(users,message.author,message.channel)
+async def on_voice_state_update(member,before,after):
+	with open('users.json','r') as f:
+		users=json.load(f)
+	if not before.channel and after.channel:
+		users[user.id]["time"] = time.time()
+	elif before.channel and not after.channel and member.id in data:
+		minutes = (time.time() - users[user.id]) // 60
+		await add_exp(users,message.author,minutes)
+	await update_data(users,message.author)
+	await level_up(users,message.author,message.channel)
 
 
 	#with open('users.json','w') as f:
@@ -113,9 +195,9 @@ async def level_up(users,user,channel):
 		users[user.id][level]=lvl.end
 
 #level lookup in specific channel
-async def check_level(users,user,channel):
-	if message.channel.id=='966066693556306010'：
-	print(users[user.id]['level']
+# async def check_level(users,user,channel):
+# 	if message.channel.id=='966066693556306010'：
+# 	print(users[user.id]['level']
 	
 
 #一些与用户的指令交互功能
